767d6f7f40872a4370f8f49b75f25a876d74ed6f
Rangat001 <rgtwork114@gmail.com>
2025-05-16 12:37:18 +0530

    Updates

--- todolist/src/main/java/com/rgt/todolist/controller/TodoController.java
+++ todolist/src/main/java/com/rgt/todolist/controller/TodoController.java
@@ -2,24 +2,21 @@ package com.rgt.todolist.controller;
 
 import com.rgt.todolist.model.Todo;
 import com.rgt.todolist.model.User;
-import com.rgt.todolist.repository.UserRepository;
+
 import com.rgt.todolist.service.TodoService;
 import com.rgt.todolist.service.UserService;
-import jakarta.persistence.Id;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-import java.time.format.DateTimeFormatter;
 
 @Controller
 @RequestMapping("/todo")
